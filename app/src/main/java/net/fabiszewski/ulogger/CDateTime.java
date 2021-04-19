1663d95f3a0871aec2b6a1149fa16c03b8b44bef
stevegdavis <stevedavis@post.com>
2021-04-19 12:49:56 +0100

    Removed some helper methods to CDateTime class
    Methods for use also in new KML export service

--- app/src/main/java/net/fabiszewski/ulogger/CDateTime.java
+++ app/src/main/java/net/fabiszewski/ulogger/CDateTime.java
@@ -1,6 +1,8 @@
 package net.fabiszewski.ulogger;
 
 
+import android.util.Log;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -65,6 +67,82 @@ public class CDateTime
                 long elapsedSeconds = (diffDays * (1440 * 60)) + (diffHours * (60 * 60)) + ((diffMinutes * 60)) + diffSeconds;
 		return elapsedSeconds;
 	}
+
+    public static String calculateTimeTaken(Date d1, Date d2, int addOrSubtractHour) {
+        //HH converts hour in 24 hours format (0-23), day calculation
+
+        //Date d1 = new Date(date1);
+        //Date d2 = new Date(date2);
+
+        long diff;
+        long diffSeconds = 0;
+        long diffMinutes = 0;
+        long diffHours = 0;
+        long diffDays = 0;
+        try {
+            // TEST ONLY
+            //String dateStart = "31/12/2013 11:01";
+            //String dateStop = "01/08/2014 11:10";
+            //d1 = format.parse(dateStart);
+            //d2 = format.parse(dateStop);
+            //
+            // May need to calculate daylight saving time add or subtract 1 hour GMT/BST
+            //d1 = format.parse(firstData.getTimeStamp());
+            //d2 = format.parse(lastData.getTimeStamp());
+
+            //in milliseconds
+            diff = d2.getTime() - d1.getTime();
+            diffSeconds = diff / 1000;
+            diffMinutes = diffSeconds / 60;// % 60;
+            diffSeconds = diffSeconds % 60;
+
+            diffMinutes = diff / (60 * 1000) % 60;
+            diffHours = diff / (60 * 60 * 1000) % 24;
+            diffDays = diff / (24 * 60 * 60 * 1000);
+
+            switch (addOrSubtractHour) {
+                case 1:
+                    diffHours++;
+                    if (diffHours > 23) {
+                        diffDays++;
+                        diffHours = 0;
+                    }
+                    break;
+                case -1:
+                    if (diffHours > 0)
+                        diffHours--;
+                    else {
+                        diffHours = 23;
+                        diffDays--;
+                    }
+                    break;
+            }
+        }
+        catch (Exception e) {
+            if (Logger.DEBUG) { Log.d(CGPSCoordinatesGPXHandler.TAG, "[export gpx write exception: " + e + "]"); };
+        }
+        return String.valueOf((int) diffDays) + " Days, " + String.valueOf((int) Math.rint(diffHours)) + " Hours, " + String.valueOf(diffMinutes) + " Mins, " + String.valueOf(diffSeconds) + " Secs";
+    }
+
+    public static int adjustDaylightSaving(Date firstDT, Date lastDT, Calendar calLastSunInMarch, Calendar calLastSunInOctober) {
+
+        //Date firstDT = new Date(firstTime);
+        //Date lastDT = new Date(lastTime);
+        //SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        Calendar cal1 = Calendar.getInstance(Locale.UK);
+        Calendar cal2 = Calendar.getInstance(Locale.UK);
+        cal1.setTime(firstDT);
+        cal2.setTime(lastDT);
+        int result = 0;
+        if (cal1.before(calLastSunInMarch) && (cal2.after(calLastSunInMarch) && (cal2.before(calLastSunInOctober))))
+            result = -1;  // GMT to BST
+        else if (cal1.before(calLastSunInMarch) && cal2.after(calLastSunInOctober))
+            result = 0;   // GMT to GMT
+        else if ((cal1.after(calLastSunInMarch) && cal1.before(calLastSunInOctober)) && cal2.after(calLastSunInOctober))
+            result = 1;   // BST to GMT
+        return result;
+    }
 	
 }
 
--- app/src/main/java/net/fabiszewski/ulogger/CGPSCoordinatesGPXHandler.java
+++ app/src/main/java/net/fabiszewski/ulogger/CGPSCoordinatesGPXHandler.java
@@ -12,6 +12,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
+import org.w3c.dom.CDATASection;
+
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -24,7 +26,7 @@ import static net.fabiszewski.ulogger.SettingsActivity.KEY_DEVICENAME;
 
 public class CGPSCoordinatesGPXHandler
 {
-    private static final String TAG = GpxExportService.class.getSimpleName();
+    public static final String TAG = GpxExportService.class.getSimpleName();
     // namespace for use with uLogger-server(from ulogger)
 	public static String ns = "<gpx xmlns=\"http://www.topografix.com/GPX/1/1\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd https://github.com/stevegdavis/ulogger-android/1 https://raw.githubusercontent.com/stevegdavis/uLogger-server/master/scripts/gpx_extensions1.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:ulogger=\"https://github.com/stevegdavis/uLogger-android/1\" creator=\"uLogger-server 1.0-beta\" version=\"1.0\">";
     private ProgressDialog progress = null;
@@ -51,7 +53,7 @@ public class CGPSCoordinatesGPXHandler
             int Idx = 0;
             startCursor.moveToFirst();
             while (cursor.moveToNext()) {
-                String timeTakenString = calculateTimeTaken(DbAccess.getDate(startCursor), DbAccess.getDate(cursor), adjustDaylightSaving(DbAccess.getDate(cursor), DbAccess.getDate(startCursor), calM, calO)); // 0 = no add or subtract, 1 = add, -1 = subtract 1 hour
+                String timeTakenString = CDateTime.calculateTimeTaken(DbAccess.getDate(startCursor), DbAccess.getDate(cursor), CDateTime.adjustDaylightSaving(DbAccess.getDate(cursor), DbAccess.getDate(startCursor), calM, calO)); // 0 = no add or subtract, 1 = add, -1 = subtract 1 hour
                 outputStream.write(("<wpt lat=" + "\"" + DbAccess.getLatitude(cursor) + "\"" + " lon=" + "\"" + DbAccess.getLongitude(cursor) + "\"" + ">").getBytes());
                 // <ele>88.4000015258789</ele>
                 outputStream.write(("<ele>" + String.valueOf(DbAccess.getAltitude(cursor)) + "</ele>").getBytes());
@@ -92,82 +94,6 @@ public class CGPSCoordinatesGPXHandler
     private String capitalise(String s)
     {
         return Character.toUpperCase(s.charAt(0)) + s.substring(1);
-    }
-
-    private String calculateTimeTaken(Date d1, Date d2, int addOrSubtractHour) {
-        //HH converts hour in 24 hours format (0-23), day calculation
-
-        //Date d1 = new Date(date1);
-        //Date d2 = new Date(date2);
-
-        long diff;
-        long diffSeconds = 0;
-        long diffMinutes = 0;
-        long diffHours = 0;
-        long diffDays = 0;
-        try {
-            // TEST ONLY
-            //String dateStart = "31/12/2013 11:01";
-            //String dateStop = "01/08/2014 11:10";
-            //d1 = format.parse(dateStart);
-            //d2 = format.parse(dateStop);
-            //
-            // May need to calculate daylight saving time add or subtract 1 hour GMT/BST
-            //d1 = format.parse(firstData.getTimeStamp());
-            //d2 = format.parse(lastData.getTimeStamp());
-
-            //in milliseconds
-            diff = d2.getTime() - d1.getTime();
-            diffSeconds = diff / 1000;
-            diffMinutes = diffSeconds / 60;// % 60;
-            diffSeconds = diffSeconds % 60;
-
-            diffMinutes = diff / (60 * 1000) % 60;
-            diffHours = diff / (60 * 60 * 1000) % 24;
-            diffDays = diff / (24 * 60 * 60 * 1000);
-
-            switch (addOrSubtractHour) {
-                case 1:
-                    diffHours++;
-                    if (diffHours > 23) {
-                        diffDays++;
-                        diffHours = 0;
-                    }
-                    break;
-                case -1:
-                    if (diffHours > 0)
-                        diffHours--;
-                    else {
-                        diffHours = 23;
-                        diffDays--;
-                    }
-                    break;
-            }
-        }
-        catch (Exception e) {
-            if (Logger.DEBUG) { Log.d(TAG, "[export gpx write exception: " + e + "]"); };
-        }
-        return String.valueOf((int) diffDays) + " Days, " + String.valueOf((int) Math.rint(diffHours)) + " Hours, " + String.valueOf(diffMinutes) + " Mins, " + String.valueOf(diffSeconds) + " Secs";
-    }
-
-    private int adjustDaylightSaving(Date firstDT, Date lastDT, Calendar calLastSunInMarch, Calendar calLastSunInOctober) {
-
-        //Date firstDT = new Date(firstTime);
-        //Date lastDT = new Date(lastTime);
-        //SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-
-        Calendar cal1 = Calendar.getInstance(Locale.UK);
-        Calendar cal2 = Calendar.getInstance(Locale.UK);
-        cal1.setTime(firstDT);
-        cal2.setTime(lastDT);
-        int result = 0;
-        if (cal1.before(calLastSunInMarch) && (cal2.after(calLastSunInMarch) && (cal2.before(calLastSunInOctober))))
-            result = -1;  // GMT to BST
-        else if (cal1.before(calLastSunInMarch) && cal2.after(calLastSunInOctober))
-            result = 0;   // GMT to GMT
-        else if ((cal1.after(calLastSunInMarch) && cal1.before(calLastSunInOctober)) && cal2.after(calLastSunInOctober))
-            result = 1;   // BST to GMT
-        return result;
     }
 }
 
